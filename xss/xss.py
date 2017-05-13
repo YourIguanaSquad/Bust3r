68ac2f83b962f4378d43da81c51e4821d8d822b9
LucidScorpion <CyberCored@protonmail.com>
2017-05-13 03:18:20 -0400

    Update xss.py

--- xss/xss.py
+++ xss/xss.py
@@ -5,11 +5,6 @@ from sgmllib import SGMLParser
 import os, sys, urllib, httplib, re, urllib2, sets, socket, subprocess, ConfigParser
 from termcolor import colored
 
-if not os.geteuid() == 0:
-	sys.exit('Script must be run as root')
-
-os.system('cls' if os.name == 'nt' else 'clear')
-
 xss = (
 	"<SCrIpT>alert('XSS')</ScRiPt>",
 	"%22%3Cscript%3Ealert%28%27XSS%27%29%3C%2Fscript%3E",
